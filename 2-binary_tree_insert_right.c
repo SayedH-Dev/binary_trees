976f0b9441d3750092294f2a1c870556cab0fb5f
SayedH-Dev <eng.elsayedfawzy@gmail.com>
2023-11-30 02:35:09 -0800

    t1and2bf

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,13 +15,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	if (new_node == NULL)
-		return (NULL);
-
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
+
+	if (new_node == NULL)
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
