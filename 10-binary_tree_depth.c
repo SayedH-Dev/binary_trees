62dbb0f305b6b8bc2d08e2f71b7e8d2cf259183a
ddedo2468 <58207352+ddedo2468@users.noreply.github.com>
2023-11-30 13:44:17 +0200

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - get the depth of a node in a binary tree
+ * @tree: A ptr to the node.
+ * Return: Depth of the node.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL || tree->parent == NULL)
+		return 0;
+
+	return 1 + binary_tree_depth(tree->parent);
+}
